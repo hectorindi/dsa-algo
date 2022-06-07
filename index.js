46494eb64d05bf930feaa8c4ac2e8697a3098b15
hectorindi <piyushnegi1989@gmail.com>
2022-06-07 11:07:39 +0530

    selection sort

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 function insertionSort(arr) {
-    //j i 
-    //[1,2,3,5,6,8]  
     for (let i = 1; i < arr.length; i++) {
         let j = i - 1;
         while (j >= 0 && arr[j] > arr[i]) {
@@ -12,3 +10,20 @@ function insertionSort(arr) {
     return arr;
 }
 console.log(insertionSort([6, 8, 24, 7, 9, 33, 66, 11, 22, 44, 5, 3, 1, 2]));
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let indexMin = 0;
+        let min = arr[i];
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[j] < min) {
+                //min = arr[j];
+                indexMin = j;
+                changed = true;
+                ([arr[i], arr[indexMin]] = [arr[indexMin], arr[i]]);
+            }
+        }
+    }
+    return arr;
+}
+console.log(selectionSort([6, 8, 24, 7, 9, 33, 66, 11, 22, 44, 5, 3, 1, 2]));
